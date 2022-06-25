fbf3373a4269cf4091559b5edddb453ee7dd7df6
Vladimir Gordiychuk <Vladimir.Gordiychuk@gmail.com>
2022-06-25 07:57:03 +0200

    client : Extracted QuestionForm component to be reused in QuestionEdit page. Added corresponding route '/questions/:id/edit'.

--- Quiz/ClientApp/src/components/App.js
+++ Quiz/ClientApp/src/components/App.js
@@ -1,18 +1,23 @@
-import './custom.css'
+import "./custom.css";
 
-import React from 'react';
+import React from "react";
 import { Routes, Route } from "react-router-dom";
-import { Layout } from './Layout';
-import { ApplicationPaths, LoginActions, LogoutActions } from './api-authorization/ApiAuthorizationConstants';
+import { Layout } from "./Layout";
+import {
+    ApplicationPaths,
+    LoginActions,
+    LogoutActions,
+} from "./api-authorization/ApiAuthorizationConstants";
 
 import Logo from "./Logo";
 import QuestionList from "./questions/QuestionList";
 import Question from "./questions/Question";
 import QuestionDelete from "./questions/QuestionDelete";
 import QuestionNew from "./questions/QuestionNew";
+import QuestionEdit from "./questions/QuestionEdit";
 
-import { Login } from './api-authorization/Login';
-import { Logout } from './api-authorization/Logout';
+import { Login } from "./api-authorization/Login";
+import { Logout } from "./api-authorization/Logout";
 
 export default class App extends React.Component {
     static displayName = App.name;
@@ -23,21 +28,50 @@ export default class App extends React.Component {
                 <Routes>
                     <Route path="/questions" element={<QuestionList />} />
                     <Route path="/questions/new" element={<QuestionNew />} />
+
                     <Route path="/questions/:id" element={<Question />} />
+                    <Route
+                        path="/questions/:id/edit"
+                        element={<QuestionEdit />}
+                    />
                     <Route
                         path="/questions/:id/delete"
                         element={<QuestionDelete />}
                     />
                     <Route path="/" element={<Logo />} />
                     <Route path={ApplicationPaths.ApiAuthorizationPrefix}>
-                        <Route path={ApplicationPaths.Login} element={loginAction(LoginActions.Login)} />
-                        <Route path={ApplicationPaths.LoginFailed} element={loginAction(LoginActions.LoginFailed)} />
-                        <Route path={ApplicationPaths.LoginCallback} element={loginAction(LoginActions.LoginCallback)} />
-                        <Route path={ApplicationPaths.Profile} element={loginAction(LoginActions.Profile)} />
-                        <Route path={ApplicationPaths.Register} element={loginAction(LoginActions.Register)} />
-                        <Route path={ApplicationPaths.LogOut} element={logoutAction(LogoutActions.Logout)} />
-                        <Route path={ApplicationPaths.LogOutCallback} element={logoutAction(LogoutActions.LogoutCallback)} />
-                        <Route path={ApplicationPaths.LoggedOut} element={logoutAction(LogoutActions.LoggedOut)} />
+                        <Route
+                            path={ApplicationPaths.Login}
+                            element={loginAction(LoginActions.Login)}
+                        />
+                        <Route
+                            path={ApplicationPaths.LoginFailed}
+                            element={loginAction(LoginActions.LoginFailed)}
+                        />
+                        <Route
+                            path={ApplicationPaths.LoginCallback}
+                            element={loginAction(LoginActions.LoginCallback)}
+                        />
+                        <Route
+                            path={ApplicationPaths.Profile}
+                            element={loginAction(LoginActions.Profile)}
+                        />
+                        <Route
+                            path={ApplicationPaths.Register}
+                            element={loginAction(LoginActions.Register)}
+                        />
+                        <Route
+                            path={ApplicationPaths.LogOut}
+                            element={logoutAction(LogoutActions.Logout)}
+                        />
+                        <Route
+                            path={ApplicationPaths.LogOutCallback}
+                            element={logoutAction(LogoutActions.LogoutCallback)}
+                        />
+                        <Route
+                            path={ApplicationPaths.LoggedOut}
+                            element={logoutAction(LogoutActions.LoggedOut)}
+                        />
                     </Route>
                 </Routes>
             </Layout>
@@ -46,9 +80,9 @@ export default class App extends React.Component {
 }
 
 function loginAction(name) {
-    return (<Login action={name}></Login>);
+    return <Login action={name}></Login>;
 }
 
 function logoutAction(name) {
-    return (<Logout action={name}></Logout>);
+    return <Logout action={name}></Logout>;
 }
--- Quiz/ClientApp/src/components/questions/QuestionEdit.js
+++ Quiz/ClientApp/src/components/questions/QuestionEdit.js
@@ -1,3 +1,33 @@
-function QuestionEdit() {}
+import { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { useParams } from "react-router-dom";
+
+import QuestionForm from "./QuestionForm";
+import { fetchQuestion, updateQuestion } from "../../actions";
+
+function QuestionEdit() {
+    const { id } = useParams();
+    const dispatch = useDispatch();
+    const question = useSelector(({ questions }) => questions[id]);
+
+    useEffect(() => {
+        if (!question && id) {
+            dispatch(fetchQuestion(id));
+        }
+    }, []);
+
+    if (!question) {
+        return <div>Loading...</div>;
+    }
+
+    console.log(question);
+
+    const onSubmit = (values) => {
+        values.id = id;
+        dispatch(updateQuestion(values));
+    };
+
+    return <QuestionForm initialValues={question} onSubmit={onSubmit} />;
+}
 
 export default QuestionEdit;
