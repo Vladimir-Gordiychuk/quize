c497835ed78dd8069bde84c05bbcac3ad972f01f
Vladimir Gordiychuk <Vladimir.Gordiychuk@gmail.com>
2022-06-25 08:51:46 +0200

    client : QuestionNew page is using QuestionForm too (reduced code duplication).

--- Quiz/ClientApp/src/components/questions/QuestionNew.js
+++ Quiz/ClientApp/src/components/questions/QuestionNew.js
@@ -1,134 +1,20 @@
-import React, { useState } from "react";
+import React from "react";
 import { useDispatch } from "react-redux";
-import { Form, Field } from "react-final-form";
 import { createQuestion } from "../../actions";
 
+import QuestionForm from "./QuestionForm";
+
 export default function QuestionNew() {
-    const [text, setText] = useState("");
-    const [options, setOptions] = useState([]);
     const dispatch = useDispatch();
 
     const onSubmit = (values) => {
         dispatch(createQuestion(values));
     };
 
-    const validate = (question) => {
-        const errors = {};
-        if (!question.text) {
-            errors.text = "Question cannot be empty.";
-        }
-        if (!question.options || question.options.length < 2) {
-            //errors.options = "At least one option must be specified.";
-        } else {
-            errors.options = question.options.map((option) =>
-                option.text ? {} : { text: "Option cannot be empty." }
-            );
-        }
-        return errors;
-    };
-
-    const renderField = ({ input, meta, label }) => (
-        <div className="field">
-            <label>{label}</label>
-            <input
-                {...input}
-                type="text"
-                placeholder="Type your question here."
-            />
-            {meta.error && meta.touched && (
-                <div className="ui error message">
-                    <div className="header">{meta.error}</div>
-                </div>
-            )}
-        </div>
-    );
-
-    const renderOption = (index) => (
-        <div className="field" key={index}>
-            <label>Option #{index + 1}</label>
-            <div className="fields">
-                <div className="two wide field" style={{ marginTop: "10px" }}>
-                    <div className="ui checkbox">
-                        <Field
-                            name={`options[${index}].correct`}
-                            type="checkbox"
-                        >
-                            {({ input }) => (
-                                <input type="checkbox" {...input} />
-                            )}
-                        </Field>
-                        <label>Is True</label>
-                    </div>
-                </div>
-                <div className="eight wide field">
-                    <Field name={`options[${index}].text`} type="text">
-                        {({ input, meta }) => (
-                            <React.Fragment>
-                                <input type="text" {...input} />
-                                {meta.error && meta.touched && (
-                                    <div className="ui error message">
-                                        <div className="header">
-                                            {meta.error}
-                                        </div>
-                                    </div>
-                                )}
-                            </React.Fragment>
-                        )}
-                    </Field>
-                </div>
-            </div>
-        </div>
-    );
-
-    const items = options.map((option, index) => renderOption(index));
-
-    const addOption = (formValues) => {
-        setText(formValues.text);
-        setOptions([
-            ...formValues.options,
-            {
-                text: "",
-                correct: false,
-            },
-        ]);
-    };
-
-    const renderBody = ({
-        handleSubmit,
-        form,
-        submitting,
-        pristine,
-        error,
-        meta,
-        submitFailed,
-        values,
-    }) => {
-        return (
-            <form
-                className={"ui form " + (submitFailed ? "error" : "")}
-                onSubmit={handleSubmit}
-            >
-                <Field name="text" label="Question content">
-                    {renderField}
-                </Field>
-                {items}
-                <div className="ui button" onClick={() => addOption(values)}>
-                    Add Options
-                </div>
-                <button type="submit" className="ui button">
-                    Submit
-                </button>
-            </form>
-        );
-    };
-
     return (
-        <Form
-            subscription={{ submitFailed: true, values: true }}
+        <QuestionForm
             onSubmit={onSubmit}
-            validate={validate}
-            render={renderBody}
-            initialValues={{ text, options }}
+            initialValues={{ text: "", options: [] }}
         />
     );
 }
