1851e03b3fc5c5760d0b57cda819c2d433d78dc4
Vladimir Gordiychuk <Vladimir.Gordiychuk@gmail.com>
2022-06-25 16:40:57 +0200

    Dtos : added QuizDto and AttemptDto.

---
+++ Quiz/Dtos/AttemptDto.cs
@@ -0,0 +1,16 @@
+﻿using Quiz.Models;
+
+namespace Quiz.Dtos
+{
+    public class AttemptDto
+    {
+
+        public int Id { get; set; }
+
+        public QuizDto Quiz { get; set; }
+
+        public DateTime Start { get; set; }
+
+        public DateTime? Finish { get; set; }
+    }
+}
---
+++ Quiz/Dtos/QuizDto.cs
@@ -0,0 +1,15 @@
+﻿namespace Quiz.Dtos
+{
+    public class QuizDto
+    {
+        public int? Id { get; set; }
+
+        public string Title { get; set; }
+
+        public int Hash { get; set; }
+
+        public int TimeLimit { get; set; }
+
+        public List<QuestionDto> Questions { get; set; }
+    }
+}
