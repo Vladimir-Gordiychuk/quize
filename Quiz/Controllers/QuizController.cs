bcf3240bd09b42bbac85d3d9c627c3636a653ec1
Vladimir Gordiychuk <Vladimir.Gordiychuk@gmail.com>
2022-07-04 13:02:53 +0200

    QuizController : implemented GET by id action.

--- Quiz/Controllers/QuizController.cs
+++ Quiz/Controllers/QuizController.cs
@@ -1,13 +1,18 @@
 ﻿using Microsoft.AspNetCore.Mvc;
-
-// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+using Quiz.Data;
+using Quiz.Dtos;
+using System.Diagnostics;
+using System.Security.Claims;
 
 namespace Quiz.Controllers
 {
-    [Route("api/[controller]")]
+    [Route("api/quizzes")]
     [ApiController]
     public class QuizController : ControllerBase
     {
+        readonly ApplicationDbContext _db;
+        readonly DtoBuilder _builder;
+
         // GET: api/<QuizController>
         [HttpGet]
         public IEnumerable<string> Get()
@@ -15,29 +20,51 @@ namespace Quiz.Controllers
             return new string[] { "value1", "value2" };
         }
 
-        // GET api/<QuizController>/5
+        // GET api/quizzes/5
         [HttpGet("{id}")]
-        public string Get(int id)
+        public ActionResult<QuizDto> Get(int id)
         {
-            return "value";
+            var quiz = _db.Quizzes.Find(id);
+
+            if (quiz is null)
+            {
+                return NotFound();
+            }
+
+            var userId = GetCurrentUserId();
+            if (quiz.AuthorId != userId)
+            {
+                return Forbid();
+            }
+
+            return _builder.BuildQuiz(quiz);
         }
 
-        // POST api/<QuizController>
+        // POST api/quizzes
         [HttpPost]
-        public void Post([FromBody] string value)
+        public void Post(NewQuizDto newQuiz)
         {
         }
 
-        // PUT api/<QuizController>/5
+        // PUT api/quizzes/5
         [HttpPut("{id}")]
         public void Put(int id, [FromBody] string value)
         {
         }
 
-        // DELETE api/<QuizController>/5
+        // DELETE api/quizzes/5
         [HttpDelete("{id}")]
         public void Delete(int id)
         {
+        }
+
+        private string GetCurrentUserId()
+        {
+            var claimsIdentity = User.Identity as ClaimsIdentity;
+            Debug.Assert(claimsIdentity != null, "User is required to be logged in.");
+            var claim = claimsIdentity.FindFirst(ClaimTypes.NameIdentifier);
+            Debug.Assert(claim != null, "All valid users are supposed to have an Id.");
+            return claim.Value;
         }
     }
 }
